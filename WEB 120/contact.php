6f91b02fb12921ee88e51898568f75fbeed1cc38
amirkiani <53153060+amirkiani@users.noreply.github.com>
2020-05-02 19:57:46 -0700

    Upload of portal/contact page
    Includes the updated portal page and contact page with ReCaptcha

---
+++ WEB 120/contact.php
@@ -0,0 +1,49 @@
+ <link rel="stylesheet" href="css/forms.css" />
+
+<!-- see Bill's training video to learn how to add and use a config.php file -->
+
+<?php include "includes/header.php";?>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="Picure of Ameer" />
+ <img src="images/phone.jpg" class="phone" alt="Picture of Ameer at Leavenworth" />
+<p><b>Ciao everyone! My name is Ameer Kiani.</b> I moved to Seattle in April 2019. After working with several non-profit organizations and studying business administration, I have decided to make a career change. It may seem daunting at first to jump into a new field but I am excited for the new possibilities.</p>
+
+<p>This is my second quarter here at SCC. So far, I find the web development track to be the most captivating. I previously used to run some websites and message boards in my teens so web development has always been a passion of mine. Some of my hobbies are learning about astrology, reading graphic novels (with my cat Fiona curled up on my lap), and dancing. </p>
+
+<p>I am a Libra-Scorpio cusp so I am a passionate, honest person who is also searching for equality. I love researching new topics but can come across as a "know-it-all" or brutally honest, but that is not my intention. Pet peeves of mine are dishonesty and superficiality. </p>
+</section>
+
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Right Column</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="Picture of Ameer at Gasworks Park" />
+
+</aside>
+<?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
+
+        include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress = "ameerkiani@icloud.com";  //place your/your client's email address here
+        $toName = "Hamza Kiani"; //place your client's name here
+        $website = "ameercodes.com/web120";  //place NAME of your client's website
+
+        echo loadContact('simple.php');#demonstrates a simple contact form
+        //echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	?>
+
+<!-- END RIGHT COL -->
+ 
+<?php include "includes/footer.php";?>
+
+
+
---
+++ WEB 120/index.php
@@ -0,0 +1,28 @@
+<!-- see Bill's training video to learn how to add and use a config.php file -->
+
+<?php include "includes/header.php";?>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="Picure of Ameer" />
+ <img src="images/phone.jpg" class="phone" alt="Picture of Ameer at Leavenworth" />
+<p><b>Ciao everyone! My name is Ameer Kiani.</b> I moved to Seattle in April 2019. After working with several non-profit organizations and studying business administration, I have decided to make a career change. It may seem daunting at first to jump into a new field but I am excited for the new possibilities.</p>
+
+<p>This is my second quarter here at SCC. So far, I find the web development track to be the most captivating. I previously used to run some websites and message boards in my teens so web development has always been a passion of mine. Some of my hobbies are learning about astrology, reading graphic novels (with my cat Fiona curled up on my lap), and dancing. </p>
+
+<p>I am a Libra-Scorpio cusp so I am a passionate, honest person who is also searching for equality. I love researching new topics but can come across as a "know-it-all" or brutally honest, but that is not my intention. Pet peeves of mine are dishonesty and superficiality. </p>
+</section>
+
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Right Column</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="Picture of Ameer at Gasworks Park" />
+
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include "includes/footer.php";?>
+
+
+
