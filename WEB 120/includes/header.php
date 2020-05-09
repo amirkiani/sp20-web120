d533824f069215dbbb517c8bc1aa63c327dd7118
Ameer Kiani <53153060+amirkiani@users.noreply.github.com>
2020-05-09 02:08:16 -0700

    Update header.php

--- WEB 120/includes/header.php
+++ WEB 120/includes/header.php
@@ -1,7 +1,8 @@
+<?php include 'portal-config.php'?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>WEB120 Portal for Ameer Kiani</title>
+    <title><?=$title?></title>
     <meta name="viewport" content="width=device-width" />
     <meta name="robots" content="noindex,nofollow" />
     <meta charset="utf-8">
@@ -15,15 +16,10 @@
 <!-- START WRAPPER -->
 <main class="wrapper">
 <header>
-  <h1><a href="index.php"><i class="logo fa fa-home"></i> Ameer's WEB 120 Portal</a></h1>
+  <h1><i class="logo fa <?=$logo?>"></i> Ameer's SCC WEB120 Portal</h1>
   <nav>
     <ul class="topnav" id="myTopnav">
-      <li><a href="index.php" class="selected">Welcome</a></li>
-      <li><a href="big/index.php">Big</a></li>
-      <li><a href="aia.php">AIA</a></li>
-      <li><a href="flowchart.php">Flowchart/Layout</a></li>
-      <li><a href="fp/index.php">Final Project</a></li>
-      <li><a href="contact.php">Contact Ameer</a></li>
+        <?=makeLinks($nav1)?>  
       <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
     </ul>
   </nav>
@@ -31,4 +27,4 @@
         
 <!-- START LEFT COL -->
 <section>
- <h2 class="pageID">Welcome</h2>
+ <h2 class="pageID"><?=$PageID?></h2>
